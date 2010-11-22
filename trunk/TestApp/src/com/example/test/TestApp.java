89ad2aad81fc0f453936e07bec2b809a96369d7d
dan.di.matteo.19 <dan.di.matteo.19@a145765c-04f7-beda-da3f-7d684ae98550>
2010-11-22 03:42:42 +0000

    upgraded UI to xml layout

--- trunk/TestApp/src/com/example/test/TestApp.java
+++ trunk/TestApp/src/com/example/test/TestApp.java
@@ -2,15 +2,13 @@ package com.example.test;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import fr.lium.spkDiarization.programs.*;
 
 public class TestApp extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Test App");
-        setContentView(tv);
+        setContentView(R.layout.main);
     }
 }
