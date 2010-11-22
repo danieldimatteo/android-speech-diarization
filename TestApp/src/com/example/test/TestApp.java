72da790ad77c5bc3215153499c884f0a718eba4f
dan.di.matteo.19 <dan.di.matteo.19@a145765c-04f7-beda-da3f-7d684ae98550>
2010-11-22 04:54:11 +0000

    added button and on-click method call

--- TestApp/src/com/example/test/TestApp.java
+++ TestApp/src/com/example/test/TestApp.java
@@ -3,12 +3,24 @@ package com.example.test;
 import android.app.Activity;
 import android.os.Bundle;
 import fr.lium.spkDiarization.programs.*;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class TestApp extends Activity {
-    /** Called when the activity is first created. */
+    private Button button;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        this.setContentView(R.layout.main);
+        this.button = (Button) this.findViewById(R.id.button);
+        this.button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+              finish();
+            }
+        });
+
     }
 }
