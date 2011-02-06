3c6a3bca2ac7ece67df68d5e4165c65597e47bee
dan.di.matteo.19 <dan.di.matteo.19@a145765c-04f7-beda-da3f-7d684ae98550>
2011-02-06 03:41:46 +0000

    - cleaning up code:   -> AudioRecorder (extension of AsyncTask) now moved out to separate java file

--- FrontEnd/src/com/example/frontend/Recorder.java
+++ FrontEnd/src/com/example/frontend/Recorder.java
@@ -1,2 +1,88 @@
 package com.example.frontend;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+
+class RecordAudio extends AsyncTask<AudioRecord, Void , Void> {
+    String AUDIO_FILE = "/sdcard/recordoutput.raw";
+
+	@Override
+	protected Void doInBackground(AudioRecord... recorder) {
+		if (recorder[0] != null) {
+            recorder[0].release();
+        }
+        
+        File outFile = new File(AUDIO_FILE);
+
+        if(outFile.exists())
+        {
+            outFile.delete();
+        }
+
+        
+        int bufferSize = AudioRecord.getMinBufferSize(
+				(int)8000, 
+				AudioFormat.CHANNEL_IN_MONO, 
+				AudioFormat.ENCODING_PCM_16BIT) * 4;
+        
+
+        short[] microphoneBuffer = new short[bufferSize];
+
+        
+        recorder[0] = new AudioRecord(
+                MediaRecorder.AudioSource.VOICE_RECOGNITION,
+                8000,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize);
+
+        FileOutputStream outFileStream = null;
+		try {
+			outFileStream = new FileOutputStream(outFile);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        DataOutputStream dataOutStream = new DataOutputStream(outFileStream);
+        
+        recorder[0].startRecording();
+                	
+        while(recorder[0].getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
+	        
+        	int numSamplesRead = recorder[0].read(microphoneBuffer, 0, bufferSize);
+	        
+	        if(numSamplesRead == AudioRecord.ERROR_INVALID_OPERATION) {
+	            throw new IllegalStateException("read() returned AudioRecord.ERROR_INVALID_OPERATION");
+	        }
+	        else if(numSamplesRead == AudioRecord.ERROR_BAD_VALUE) {
+	            throw new IllegalStateException("read() returned AudioRecord.ERROR_BAD_VALUE");
+	        }
+	        
+	        for(int bufferIndex = 0; bufferIndex < numSamplesRead; bufferIndex++) {
+	        	try {
+					dataOutStream.writeShort(microphoneBuffer[bufferIndex]);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+	                
+        }
+
+        try {
+			dataOutStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
