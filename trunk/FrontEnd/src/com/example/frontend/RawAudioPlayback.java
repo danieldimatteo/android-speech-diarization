e795731f6a13b93d2dcfa82fab0a38713a5501ce
dan.di.matteo.19 <dan.di.matteo.19@a145765c-04f7-beda-da3f-7d684ae98550>
2011-02-07 03:32:46 +0000

    - can now properly play back the recorded audio

--- trunk/FrontEnd/src/com/example/frontend/RawAudioPlayback.java
+++ trunk/FrontEnd/src/com/example/frontend/RawAudioPlayback.java
@@ -1,5 +1,11 @@
 package com.example.frontend;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.media.AudioTrack;
 
 public class RawAudioPlayback {
@@ -9,8 +15,17 @@ public class RawAudioPlayback {
     int channelConfig;
     int audioFormat;
     int mode;
+
     AudioTrack track;
+    short[] buffer;
+    
+    File audioSampleFile;
+    FileInputStream fileStream;
+    DataInputStream sampleStream;
+    long numberSamples;
+    long samplesRead;
 	
+    
     public RawAudioPlayback(String audio, int audioStrm, int sampleRt, int channelCfg, int audioFrmt, int playMode ){
     	audioFile = audio;
     	audioStream = audioStrm;
@@ -20,14 +35,50 @@ public class RawAudioPlayback {
     	mode = playMode;
     	
         int bufferSize = AudioTrack.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
-    	
     	track = new AudioTrack(audioStream, sampleRateInHz, channelConfig, audioFormat, bufferSize, mode);
+    	
+    	buffer = new short[1024];
+    	
+    	audioSampleFile = new File(audio);
+    	numberSamples = audioSampleFile.length() / 2;
+    	try {
+			fileStream = new FileInputStream(audioSampleFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		sampleStream = new DataInputStream(fileStream);
+    	
     }
     
-    public void play() {
+    public void play() throws IOException {
+    	track.play();
+    	while (samplesRead < numberSamples){
+    		this.fillBuffer();
+    		track.write(buffer, 0, buffer.length);
+    	}
+    	
+    	track.flush();
+    	track.stop();
+    	track.release();
 
-		
-		
     	return;
     }
+    
+    public void fillBuffer(){
+    	int i;
+    	for (i = 0; i<buffer.length; i++  ){
+    		if (samplesRead < numberSamples){
+	    		try {
+					buffer[i] = sampleStream.readShort();
+					this.samplesRead++;
+	
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+    		}
+    	}
+    }
+    
 }
